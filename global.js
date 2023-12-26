a35d3530ba1e1d18a4061d6232b8b3e57dc91d0f
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2023-12-26 14:58:22 -0600

    Update global.js

--- global.js
+++ global.js
@@ -42,18 +42,6 @@ function update() {
 
 //abbreviate
 function abbrNum(number) {
-  if (number < 0) {
-    return "-" + abbrNum(-1 * number);
-  }
-  if (number < 1000) {
-    return number;
-  } else if (number >= 1000 && number < 1_000_000) {
-    return (number / 1000).toFixed(1).replace(/\.0$/, "") + "K";
-  } else if (number >= 1_000_000 && number < 1_000_000_000) {
-    return (number / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
-  } else if (number >= 1_000_000_000 && number < 1_000_000_000_000) {
-    return (number / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B";
-  } else if (number >= 1_000_000_000_000 && number < 1_000_000_000_000_000) {
-    return (number / 1_000_000_000_000).toFixed(1).replace(/\.0$/, "") + "T";
-  }
+  const formatter = Intl.NumberFormat("en", { notation: "compact" });
+  return formatter.format(number);
 }
