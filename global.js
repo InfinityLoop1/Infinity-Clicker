a30edeb2fd89add8d4846cbee7250a85bb2ed726
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2023-12-26 14:11:33 -0600

    Update global.js

--- global.js
+++ global.js
@@ -1,15 +1,22 @@
 let points = 0;
 let cpc = 1;
   
-  document.getElementById('button').onclick = () => {
-    points = points + cpc;
-    document.getElementById('score').innerText = points;
-  };
+document.getElementById('button').onclick = () => {
+  points = points + cpc;
+  update();
+};
 
-  document.getElementById('upgcpc').onclick = () => {
-    if (points >= cpc * 2) {
-      points = points - (cpc * 2);
-      cpc = cpc + 1;
-      document.getElementById('score').innerText = points;
-    }
-  };
+document.getElementById('upgcpc').onclick = () => {
+  if (points >= cpc * 2) {
+    points = points - (cpc * 2);
+    cpc = cpc + 1;
+    update();
+  }
+};
+
+function update() {
+  document.getElementById('score').innerText = points;
+  document.getElementById('upgcpc').innerText = "Upgrade CPC: $" + (cpc * 2);
+}
+
+
