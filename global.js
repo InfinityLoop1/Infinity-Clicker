a166e1aab2455fb3955e7da0eca0af840484a9e1
InfinityLoop <127774659+InfinityLoop1@users.noreply.github.com>
2024-01-02 13:51:01 -0600

    Update global.js

--- global.js
+++ global.js
@@ -123,10 +123,10 @@ document.getElementById("prestigebuttonboost").onclick = () => {
   cps = 0;
 };
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+const modal = document.querySelector(".prestige-modal");
+const overlay = document.querySelector(".prestige-overlay");
+const openModalBtn = document.querySelector(".prestige-btn-open");
+const closeModalBtn = document.querySelector(".prestige-btn-close");
 
 const openModal = function () {
   modal.classList.remove("hidden");
